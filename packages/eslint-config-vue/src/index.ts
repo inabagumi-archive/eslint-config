aa1834d91c5c50ad6f53d76218753c7140a5d2ef
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-08-10 23:34:34 +0900

    fix: run lint (#10)

--- packages/eslint-config-vue/src/index.ts
+++ packages/eslint-config-vue/src/index.ts
@@ -2,10 +2,7 @@ export = {
   extends: ['@inabagumi'],
   overrides: [
     {
-      extends: [
-        'plugin:vue/essential',
-        'prettier/vue'
-      ],
+      extends: ['plugin:vue/essential', 'prettier/vue'],
       files: '*.vue',
       parserOptions: {
         parser: '@typescript-eslint/parser'
